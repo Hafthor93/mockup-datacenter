62c9868cf9c9511054b5fb67aa327678d75225d3
eyjolfurben <eyjolfurben@gmail.com>
2024-01-18 22:12:11 +0000

    SECURITY

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 import Heatmap from './Heatmap';
 import MinerInfo from './MinerInfo';
 
-import CustomLogo from '/Users/hafthor/Mockup Datacenter/mockup-datacenter/src/Tecna3.png';
 
 const Container = styled.div`
   background-color: #f4f4f4;
@@ -45,18 +44,21 @@ const Home = () => <div>Home Page</div>;
 const Info = () => <div>Info Page</div>;
 const Settings = () => <div>Settings Page</div>;
 
+const Security = () => <div>Security Page</div>;
+
 const App = () => {
   return (
     <BrowserRouter>
       <Container>
         <Navbar>
-          <Logo src={CustomLogo} alt="asdf" />
+          <Logo src="your_logo_url.png" alt="Logo" />
           <NavLinks>
             <Link to="/">Home</Link>
             <Link to="/heatmap">Heatmap</Link>
             <Link to="/info">Info</Link>
             <Link to="/settings">Settings</Link>
             <Link to="/minerinfo">Miner Info</Link>
+            <Link to="/security">Security</Link> {/* Added Security Link */}
           </NavLinks>
         </Navbar>
         <Routes>
@@ -65,6 +67,7 @@ const App = () => {
           <Route path="/info" element={<Info />} />
           <Route path="/settings" element={<Settings />} />
           <Route path="/minerinfo/:id" element={<MinerInfo />} />
+          <Route path="/security" element={<Security />} /> {/* Added Security Route */}
         </Routes>
       </Container>
     </BrowserRouter>
