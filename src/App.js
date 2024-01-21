e49a673b971272e49e119909910a322d02993786
Hafthor93 <101139553+Hafthor93@users.noreply.github.com>
2024-01-21 01:25:59 +0000

    asdf

--- src/App.js
+++ src/App.js
@@ -174,7 +174,7 @@ const App = () => {
     <BrowserRouter>
       <Container>
         <Navbar>
-        <Link to="/HomePage">
+        <Link to="/">
             <Logo src="https://www.tecna.is/cdn/shop/files/Tecna_ed_2_Icelandic.png?v=1695718500&width=300.png" alt="Logo" />
           </Link>
           <NavLinks>
